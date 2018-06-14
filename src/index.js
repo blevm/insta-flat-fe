95e54cf7e6bbd677e410edf8b781fd5fecb4c3c4
blevm <mjblevins7@gmail.com>
2018-06-14 14:36:40 -0400

    mb solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
-//<div class="image-container">
-//    <img src="https://scontent-lga3-1.cdninstagram.com/vp/bd9b15079ec27c52c076e9c7792bdc04/5B992309/t51.2885-15/s640x640/sh0.08/e35/c180.0.719.719/31449135_2115995735352355_6317812590797914112_n.jpg">
-//    <p>
-//        <img data-action="like-image" data-image-id="1" class="like-button" src="./images/like.png"><br>
-//        <span id="likes-count-for-image-1">41</span>
-//    </p>
-//</div>
+const imagesURL = 'http://localhost:3000/api/v1/images'
+const likesURL = 'http://localhost:3000/api/v1/likes'
+const container = document.getElementById('container')
+const imageField = document.getElementById('post-image-form-url')
+
+function fetchImages() {
+  fetch(imagesURL).then(resp => resp.json()).then(data => appendImages(data))
+}
+
+function appendImages(imageObjs) {
+  container.innerHTML = ''
+  imageObjs.forEach(function(img) {
+    container.innerHTML += `<div class="image-container">
+       <img src="${img.url}">
+       <p>
+        <img data-action="like-image" data-image-id="${img.id}" class="like-button" src="./images/like.png"><br>
+        <span id="likes-count-for-image-${img.id}">${img.likes_count}</span>
+       </p>
+    </div>`
+  })
+}
+
+document.addEventListener('click', function(event){
+  if (event.target.className === 'like-button') {
+    addALike(event.target.dataset.imageId)
+  } else if (event.target.tagName === 'BUTTON' && event.target.type === 'submit') {
+    event.preventDefault()
+    addAnImage(imageField.value)
+    imageField.value = ''
+  }
+})
+
+function addALike(imageId) {
+  let config = {
+    method: 'POST',
+    headers: {'Content-type':'application/json', 'Data-type':'application/json'},
+    body: JSON.stringify({image_id: imageId})
+  }
+  fetch(likesURL, config).then(fetchImages)
+}
+
+function addAnImage(imageURL) {
+  let config2 = {
+    method: 'POST',
+    headers: {'Content-type':'application/json', 'Data-type':'application/json'},
+    body: JSON.stringify({url: imageURL})
+  }
+  fetch(imagesURL, config2).then(fetchImages)
+}
+
+fetchImages()
